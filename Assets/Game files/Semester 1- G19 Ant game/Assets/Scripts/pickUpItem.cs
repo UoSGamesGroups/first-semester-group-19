37578fe3ae86d00146da56f5561a6a1b87c85b3b
Ochamberlai2 <S179650@ucs.ac.uk>
2016-10-31 10:03:35 +0000

    Sprint 26/10/16
    Item can be picked up and placed down in front of or behind the player.

---
+++ Assets/Game files/Semester 1- G19 Ant game/Assets/Scripts/pickUpItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpItem : MonoBehaviour {
+
+    public GameObject item;
+    public GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        item = null;
+        player = this.gameObject;	
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            item = GameObject.FindGameObjectWithTag("Leaf");
+            item.transform.parent = player.transform;
+            item.transform.position = new Vector2(player.transform.position.x, player.transform.position.y+0.5f);
+
+        }
+
+	}
+}
---
+++ Assets/Game files/Semester 1- G19 Ant game/Assets/Scripts/placeItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class placeItem : MonoBehaviour {
+    public GameObject carriedItem;
+    public GameObject player;
+    // Use this for initialization
+    void Start () {
+        player = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //place for barrier
+	    if (antMovement.direction == 1 && Input.GetKeyDown(KeyCode.E))
+            {
+            carriedItem = GameObject.FindGameObjectWithTag("Leaf");
+            carriedItem.transform.parent = null;
+            carriedItem.transform.position = new Vector2(player.transform.position.x + 0.2f, player.transform.position.y);
+            carriedItem.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        if (antMovement.direction == 0 && Input.GetKeyDown(KeyCode.E))
+        {
+            carriedItem = GameObject.FindGameObjectWithTag("Leaf");
+            carriedItem.transform.parent = null;
+            carriedItem.transform.position = new Vector2(player.transform.position.x - 0.2f, player.transform.position.y);
+            carriedItem.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+    }
+}
